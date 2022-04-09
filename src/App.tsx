e49b54c59d487afb64706cbf6f5f747a9d9e3b6f
Eniola Ajayi <ajayieniola16@gmail.com>
2022-04-09 20:19:04 +0100

    add required providers for mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { MantineProvider } from "@mantine/core";
+import { NotificationsProvider } from "@mantine/notifications";
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  return <>Hello World!</>;
+  return (
+    <MantineProvider
+      theme={{
+        fontFamily: "Lato",
+        headings: {
+          fontFamily: "Neucha",
+        },
+      }}
+    >
+      <NotificationsProvider>
+        <Outlet />
+      </NotificationsProvider>
+    </MantineProvider>
+  );
 }
 
 export default App;
