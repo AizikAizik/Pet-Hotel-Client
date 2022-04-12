4b564e21733a255710f8c50c84059b50227b23c9
Eniola Ajayi <ajayieniola16@gmail.com>
2022-04-12 01:10:15 +0100

    make adjustment to main

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,10 +1,13 @@
-import { Container, createStyles } from "@mantine/core";
+import { Container, createStyles, Title, Text, Group } from "@mantine/core";
 import React from "react";
 import PageHeader from "../components/layout/PageHeader";
 
 const useStyles = createStyles((theme) => ({
   main: {
+    width: "100%",
+    height: "600px",
     backgroundColor: "#FFF2BD",
+    color: "#425168",
   },
 }));
 export default function Home() {
@@ -15,7 +18,7 @@ export default function Home() {
       label: "Home",
     },
     {
-      link: "/pricing",
+      link: "/about",
       label: "About Us",
     },
     {
@@ -31,7 +34,16 @@ export default function Home() {
     <>
       <PageHeader links={linkData} />
       <main className={classes.main}>
-        <Container></Container>
+        <Container sx={{ paddingTop: "5rem" }}>
+          <Group position="center" sx={{ width: "100%" }}>
+            <Title order={1} sx={{ fontSize: "max(3rem, 2rem)" }}>
+              We Take Care Of Your Loving Pets
+            </Title>
+            <Text size="md" sx={{ fontSize: "1.5rem" }}>
+              Get the best the accommodation for your pets{" "}
+            </Text>
+          </Group>
+        </Container>
       </main>
     </>
   );
