ff0d2a0b1865d49b7dc0de3617fa21550eb538fc
Eniola Ajayi <ajayieniola16@gmail.com>
2022-04-13 22:55:13 +0100

    complete structure for sign up page

--- src/routes/SignUpPage.tsx
+++ src/routes/SignUpPage.tsx
@@ -16,28 +16,29 @@ import { Link } from "react-router-dom";
 export default function SignUpPage() {
   return (
     <Container size={420} my={40}>
-      <Title align="center">Welcome back!</Title>
+      <Title align="center">Join Us!</Title>
       <Text color="dimmed" size="sm" align="center" mt={5}>
-        Do not have an account yet?{" "}
-        <Anchor size="sm" component={Link} to="/signup">
-          Create account
+        Already have an account?{" "}
+        <Anchor size="sm" component={Link} to="/login">
+          Log In
         </Anchor>
       </Text>
 
       <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-        <TextInput label="Email" placeholder="you@email.com" required />
+        <TextInput label="Full Name" placeholder="John Doe" required />
+        <TextInput label="Email" placeholder="you@email.com" mt="md" required />
         <PasswordInput
           label="Password"
           placeholder="Your password"
           required
           mt="md"
         />
-        <Group position="apart" mt="md">
-          <Checkbox label="Keep me signed in" />
-          <Anchor size="sm" component={Link} to="/">
-            Forgot password?
-          </Anchor>
-        </Group>
+        <PasswordInput
+          label="Confirm Password"
+          placeholder="Enter password again"
+          required
+          mt="md"
+        />
         <Button fullWidth mt="xl" sx={{ backgroundColor: "#425168" }}>
           Create Account
         </Button>
