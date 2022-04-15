d2e8aaf9473f2dab137496f851f31f6a8fa5b5ee
AizikAizik <isaac.ogunleye@hotmail.com>
2022-04-15 15:26:38 +0400

    added the login functionality and user state in the store

--- src/routes/LoginPage.tsx
+++ src/routes/LoginPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   TextInput,
   PasswordInput,
@@ -10,10 +10,34 @@ import {
   Container,
   Group,
   Button,
+  Loader,
+  Notification,
 } from "@mantine/core";
-import { Link } from "react-router-dom";
+import { X } from "tabler-icons-react";
+import { Link, useNavigate } from "react-router-dom";
+import { useInputState } from "@mantine/hooks";
+import { useStoreActions, useStoreState } from "../state/store";
 
 export default function LoginPage() {
+  const [email, setEmail] = useInputState("");
+  const [password, setPassword] = useInputState("");
+
+  const userSessionState = useStoreState((state) => state.userSession);
+
+  const navigate = useNavigate();
+
+  const { isLoading, userInfo, error } = userSessionState;
+
+  // this useEffect checks if the user is already logged in
+  // if user is logged in then redirect user to dashboard page automatically
+  useEffect(() => {
+    if (userInfo) {
+      navigate(`/dashboard`);
+    }
+  }, [navigate, userInfo]);
+
+  const loginAction = useStoreActions((action) => action.userSession.login);
+
   return (
     <Container size={420} my={40}>
       <Title align="center">Welcome back!</Title>
@@ -24,12 +48,27 @@ export default function LoginPage() {
         </Anchor>
       </Text>
 
+      {isLoading && <Loader />}
+      {error && (
+        <Notification icon={<X size={18} />} color="red" title="Error Occured">
+          {error}
+        </Notification>
+      )}
+
       <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-        <TextInput label="Email" placeholder="you@email.com" required />
+        <TextInput
+          label="Email"
+          placeholder="you@email.com"
+          value={email}
+          onChange={setEmail}
+          required
+        />
         <PasswordInput
           label="Password"
           placeholder="Your password"
           required
+          value={password}
+          onChange={setPassword}
           mt="md"
         />
         <Group position="apart" mt="md">
@@ -38,7 +77,12 @@ export default function LoginPage() {
             Forgot password?
           </Anchor>
         </Group>
-        <Button fullWidth mt="xl" sx={{ backgroundColor: "#425168" }}>
+        <Button
+          fullWidth
+          mt="xl"
+          sx={{ backgroundColor: "#425168" }}
+          onClick={() => loginAction({ password, email })}
+        >
           Sign in
         </Button>
       </Paper>
--- src/state/store.tsx
+++ src/state/store.tsx
@@ -1,41 +1,111 @@
 import axios from "axios";
-import { Action, action, createStore, Thunk, thunk } from "easy-peasy";
+import {
+  Action,
+  action,
+  createStore,
+  createTypedHooks,
+  Thunk,
+  thunk,
+} from "easy-peasy";
+
+interface UserInfo {
+  id: string;
+  fullName: string;
+  email: string;
+  isAdmin: boolean;
+}
+
 interface UserSession {
+  error: any | null;
   isLoggedIn: boolean;
   token: string | null;
+  isLoading: boolean;
+  userInfo: UserInfo | null;
   setToken: Action<UserSession, string>;
   setLoggedIn: Action<UserSession, boolean>;
-  login: Thunk<UserSession, {}>;
+  setUserInfo: Action<UserSession, UserInfo>;
+  setIsLoading: Action<UserSession, boolean>;
+  setError: Action<UserSession, any>;
+  login: Thunk<
+    UserSession,
+    { password: string; email: string; fullName?: string }
+  >;
   logout: Action<UserSession, null>;
 }
 interface StoreModel {
   userSession: UserSession;
 }
 
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo")!)
+  : null;
+
 const store = createStore<StoreModel>({
   userSession: {
+    // state variables
     isLoggedIn: false,
+    isLoading: false,
     token: null,
+    userInfo: userInfoFromStorage,
+    error: null,
+
+    // actions
     setToken: action((state, payload) => {
       state.token = payload;
     }),
+
     setLoggedIn: action((state, payload) => {
       state.isLoggedIn = payload;
     }),
 
+    setUserInfo: action((state, payload) => {
+      state.userInfo = payload;
+    }),
+
+    setIsLoading: action((state, payload) => {
+      state.isLoading = payload;
+    }),
+
+    setError: action((state, payload) => {
+      state.error = payload;
+    }),
+
     login: thunk(async (actions, payload) => {
-      const { data } = await axios.post(
-        "https://peaceful-garden-90498.herokuapp.com/api/users/login",
-        payload
-      );
-      console.log(data);
+      try {
+        actions.setIsLoading(true);
+        const { data } = await axios.post(
+          "https://peaceful-garden-90498.herokuapp.com/api/users/login",
+          payload
+        );
+        console.log(data);
+        actions.setIsLoading(false);
+        localStorage.setItem("userInfo", JSON.stringify(data));
+        actions.setLoggedIn(true);
+        actions.setUserInfo(data);
+        actions.setToken(data.token);
+        actions.setError(null);
+      } catch (error: any) {
+        actions.setError(
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message
+        );
+        actions.setIsLoading(false);
+      }
     }),
 
     logout: action((state, payload) => {
       state.isLoggedIn = false;
       state.token = null;
+      localStorage.removeItem("userInfo");
     }),
   },
 });
+
+const typedHooks = createTypedHooks<StoreModel>();
+
+export const useStoreActions = typedHooks.useStoreActions;
+export const useStoreDispatch = typedHooks.useStoreDispatch;
+export const useStoreState = typedHooks.useStoreState;
 
 export default store;
