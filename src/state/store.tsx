f1a8b104432650d2f5749172d0207c6875e237fd
AizikAizik <isaac.ogunleye@hotmail.com>
2022-04-15 18:06:14 +0400

    refactored the store and created seperate store models in a model directory

--- src/state/store.tsx
+++ src/state/store.tsx
@@ -1,105 +1,12 @@
-import axios from "axios";
-import {
-  Action,
-  action,
-  createStore,
-  createTypedHooks,
-  Thunk,
-  thunk,
-} from "easy-peasy";
+import { createStore, createTypedHooks } from "easy-peasy";
+import { UserModel, UserSession } from "./models/user.model";
 
-interface UserInfo {
-  id: string;
-  fullName: string;
-  email: string;
-  isAdmin: boolean;
-}
-
-interface UserSession {
-  error: any | null;
-  isLoggedIn: boolean;
-  token: string | null;
-  isLoading: boolean;
-  userInfo: UserInfo | null;
-  setToken: Action<UserSession, string>;
-  setLoggedIn: Action<UserSession, boolean>;
-  setUserInfo: Action<UserSession, UserInfo>;
-  setIsLoading: Action<UserSession, boolean>;
-  setError: Action<UserSession, any>;
-  login: Thunk<
-    UserSession,
-    { password: string; email: string; fullName?: string }
-  >;
-  logout: Action<UserSession, null>;
-}
 interface StoreModel {
   userSession: UserSession;
 }
 
-const userInfoFromStorage = localStorage.getItem("userInfo")
-  ? JSON.parse(localStorage.getItem("userInfo")!)
-  : null;
-
 const store = createStore<StoreModel>({
-  userSession: {
-    // state variables
-    isLoggedIn: false,
-    isLoading: false,
-    token: null,
-    userInfo: userInfoFromStorage,
-    error: null,
-
-    // actions
-    setToken: action((state, payload) => {
-      state.token = payload;
-    }),
-
-    setLoggedIn: action((state, payload) => {
-      state.isLoggedIn = payload;
-    }),
-
-    setUserInfo: action((state, payload) => {
-      state.userInfo = payload;
-    }),
-
-    setIsLoading: action((state, payload) => {
-      state.isLoading = payload;
-    }),
-
-    setError: action((state, payload) => {
-      state.error = payload;
-    }),
-
-    login: thunk(async (actions, payload) => {
-      try {
-        actions.setIsLoading(true);
-        const { data } = await axios.post(
-          "https://peaceful-garden-90498.herokuapp.com/api/users/login",
-          payload
-        );
-        console.log(data);
-        actions.setIsLoading(false);
-        localStorage.setItem("userInfo", JSON.stringify(data));
-        actions.setLoggedIn(true);
-        actions.setUserInfo(data);
-        actions.setToken(data.token);
-        actions.setError(null);
-      } catch (error: any) {
-        actions.setError(
-          error.response && error.response.data.message
-            ? error.response.data.message
-            : error.message
-        );
-        actions.setIsLoading(false);
-      }
-    }),
-
-    logout: action((state, payload) => {
-      state.isLoggedIn = false;
-      state.token = null;
-      localStorage.removeItem("userInfo");
-    }),
-  },
+  userSession: UserModel,
 });
 
 const typedHooks = createTypedHooks<StoreModel>();
