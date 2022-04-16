381883dcaaee19826e834c1067e68821945b7747
Eniola Ajayi <ajayieniola16@gmail.com>
2022-04-16 16:32:55 +0100

    complete footer design

--- src/components/layout/PageFooter.tsx
+++ src/components/layout/PageFooter.tsx
@@ -1,6 +1,101 @@
+import {
+  createStyles,
+  Text,
+  Container,
+  ActionIcon,
+  Group,
+} from "@mantine/core";
+import { BrandTwitter, BrandYoutube, BrandInstagram } from "tabler-icons-react";
 import React from "react";
+import { Logo } from "../shared/Logo";
 
 interface PageFooterProps {}
+const useStyles = createStyles((theme) => ({
+  footer: {
+    marginTop: 120,
+    paddingTop: theme.spacing.xl * 2,
+    paddingBottom: theme.spacing.xl * 2,
+    color: theme.white,
+    backgroundColor:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[6]
+        : theme.colors.gray[2],
+    borderTop: `1px solid ${
+      theme.colorScheme === "dark" ? theme.colors.dark[5] : theme.colors.gray[3]
+    }`,
+  },
+
+  logo: {
+    maxWidth: 200,
+
+    [theme.fn.smallerThan("sm")]: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+    },
+  },
+
+  description: {
+    marginTop: 5,
+
+    [theme.fn.smallerThan("sm")]: {
+      marginTop: theme.spacing.xs,
+      textAlign: "center",
+    },
+  },
+
+  afterFooter: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: theme.spacing.xl,
+    paddingTop: theme.spacing.xl,
+    paddingBottom: theme.spacing.xl,
+    borderTop: `1px solid ${
+      theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[2]
+    }`,
+
+    [theme.fn.smallerThan("sm")]: {
+      flexDirection: "column",
+    },
+  },
+
+  social: {
+    [theme.fn.smallerThan("sm")]: {
+      marginTop: theme.spacing.xs,
+    },
+  },
+}));
+
 export default function PageFooter({}: PageFooterProps) {
-  return <footer></footer>;
+  const { classes } = useStyles();
+  return (
+    <footer className={classes.footer}>
+      <Container>
+        <div className={classes.logo}>
+          <Logo />
+          <Text size="xs" color="dimmed" className={classes.description}>
+            Build fully functional accessible web applications faster than ever
+          </Text>
+        </div>
+      </Container>
+      <Container className={classes.afterFooter}>
+        <Text color="dimmed" size="sm">
+          © 2020 mantine.dev. All rights reserved.
+        </Text>
+
+        <Group spacing={0} className={classes.social} position="right" noWrap>
+          <ActionIcon size="lg">
+            <BrandTwitter size={18} />
+          </ActionIcon>
+          <ActionIcon size="lg">
+            <BrandYoutube size={18} />
+          </ActionIcon>
+          <ActionIcon size="lg">
+            <BrandInstagram size={18} />
+          </ActionIcon>
+        </Group>
+      </Container>
+    </footer>
+  );
 }
