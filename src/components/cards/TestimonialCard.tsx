fe89a49ae5322d521437c05c05f66ee7f31922f7
Eniola Ajayi <ajayieniola16@gmail.com>
2022-04-20 20:32:51 +0100

    set title as label for divider

--- src/components/cards/TestimonialCard.tsx
+++ src/components/cards/TestimonialCard.tsx
@@ -47,15 +47,17 @@ export default function TestimonialCard(props: TestimonialCardProps) {
 
       <Group position={"center"} className={classes.card__content}>
         <Divider
-          sx={{ width: "50px" }}
           variant="dashed"
           size={"sm"}
           my="sm"
           color={"teal"}
+          labelPosition="center"
+          label={
+            <Title order={4} className={classes.card__name}>
+              {props.name}
+            </Title>
+          }
         />
-        <Title order={4} className={classes.card__name}>
-          {props.name}
-        </Title>
         <Text className={classes.card__body}>{props.body}</Text>
         <Divider
           sx={{ width: "50px" }}
