de154e6d4224ff1c195f41b48b6cdb4d915ed816
Eniola Ajayi <ajayieniola16@gmail.com>
2022-04-21 23:42:55 +0100

    create hotel details page

---
+++ src/routes/HotelDetails.tsx
@@ -0,0 +1,10 @@
+import { Group, Title } from "@mantine/core";
+import React from "react";
+
+export default function HotelDetails() {
+  return (
+    <Group>
+      <Title>Hotel Details</Title>
+    </Group>
+  );
+}
