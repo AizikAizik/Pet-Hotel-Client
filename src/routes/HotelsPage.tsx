d30ca460899f2ce9c9eb9ab3130fd3c9280c69ce
Eniola Ajayi <ajayieniola16@gmail.com>
2022-04-21 23:42:40 +0100

    create hotels page

---
+++ src/routes/HotelsPage.tsx
@@ -0,0 +1,10 @@
+import { Center, Container } from "@mantine/core";
+import React from "react";
+
+export default function HotelsPage() {
+  return (
+    <Container>
+      <Center>No Hotels yet!</Center>
+    </Container>
+  );
+}
