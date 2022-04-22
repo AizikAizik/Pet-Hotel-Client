5ef48d361d8b360d410f68e566f4af8beb203a8a
Eniola Ajayi <ajayieniola16@gmail.com>
2022-04-22 02:22:11 +0100

    render hotels in hotel page

--- src/routes/HotelsPage.tsx
+++ src/routes/HotelsPage.tsx
@@ -1,10 +1,38 @@
-import { Center, Container } from "@mantine/core";
-import React from "react";
+import { Center, Container, Grid, Title } from "@mantine/core";
+import { Actions, State, useStoreActions, useStoreState } from "easy-peasy";
+import React, { useEffect } from "react";
+import HotelCard from "../components/cards/HotelCard";
+import { StoreModel } from "../state/store";
 
 export default function HotelsPage() {
+  const fetchHotels = useStoreActions(
+    (actions: Actions<StoreModel>) => actions.hotelsState.fetchAll
+  );
+  const hotels = useStoreState(
+    (state: State<StoreModel>) => state.hotelsState.hotels
+  );
+
+  useEffect(() => {
+    fetchHotels();
+  }, [fetchHotels]);
+
   return (
-    <Container>
-      <Center>No Hotels yet!</Center>
+    <Container mt={"xl"} mb={"xl"} sx={{ minHeight: 550, height: "auto" }}>
+      {hotels.length !== 0 ? (
+        <Grid align={"stretch"} gutter={"md"}>
+          {hotels.map((hotel, idx) => {
+            return (
+              <Grid.Col sm={6} lg={4} xl={3} key={idx}>
+                <HotelCard {...hotel} key={idx} />
+              </Grid.Col>
+            );
+          })}
+        </Grid>
+      ) : (
+        <Center>
+          <Title order={1}>No Hotels Available</Title>
+        </Center>
+      )}
     </Container>
   );
 }
