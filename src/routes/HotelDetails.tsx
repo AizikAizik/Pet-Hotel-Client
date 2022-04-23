78cd8900d26fb23023b4f0d219e3c1a96345d569
Eniola Ajayi <ajayieniola16@gmail.com>
2022-04-23 03:25:26 +0100

    add support for comments and packages

--- src/routes/HotelDetails.tsx
+++ src/routes/HotelDetails.tsx
@@ -1,14 +1,59 @@
-import { Container, Title, Text, Group, Grid, Image, Box } from "@mantine/core";
+import {
+  Container,
+  Title,
+  Text,
+  Group,
+  Grid,
+  Image,
+  Box,
+  Stack,
+  Space,
+  Button,
+  useMantineTheme,
+  createStyles,
+  Card,
+  Paper,
+} from "@mantine/core";
 import axios, { AxiosRequestConfig } from "axios";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { ArrowNarrowLeft } from "tabler-icons-react";
 import { Hotel } from "../state/models/hotel.model";
 
 // interface HotelDetailsProps {}
+const useStyles = createStyles((theme) => ({
+  detail__wrapper: {
+    width: "100%",
+  },
+  detail__hero: {
+    width: "100%",
+    minHeight: 500,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    borderRadius: 8,
+    position: "relative",
+    color: theme.white,
+    "&>*": {
+      zIndex: "2",
+    },
+    "&::after": {
+      content: "''",
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      borderRadius: 8,
+      backgroundImage: "linear-gradient(#0000006a,#000000)",
+    },
+  },
+}));
 
 export default function HotelDetails() {
   let params = useParams();
-  const [hotel, setHotel] = useState<any>();
+  const theme = useMantineTheme();
+  const { classes } = useStyles();
+  const [hotel, setHotel] = useState<Hotel>();
 
   const fetchHotelById = async (id: string) => {
     try {
@@ -43,6 +88,8 @@ export default function HotelDetails() {
               width={100}
               height={100}
               withPlaceholder={true}
+              radius={8}
+              sx={{}}
             />
           );
         })}
@@ -52,15 +99,128 @@ export default function HotelDetails() {
 
   return (
     <Container my="xl">
+      <Button
+        component={Link}
+        to="/hotels"
+        variant="subtle"
+        leftIcon={<ArrowNarrowLeft />}
+        color="gray"
+      >
+        Back
+      </Button>
       {hotel ? (
-        <Box>
-          <Title>{hotel.name}</Title>
-          <Text>{hotel.description}</Text>
-          <Group>
-            <Text>rating:{hotel.ratings}</Text>
-            <Text>Rooms Available{hotel.roomsAvailable}</Text>
-          </Group>
-          {createImageGrid(hotel)}
+        <Box py="xl" className={classes.detail__wrapper}>
+          <Stack
+            align="flex-start"
+            justify={"center"}
+            px={"xl"}
+            spacing="sm"
+            className={classes.detail__hero}
+            style={{ backgroundImage: `url(${hotel.images[0]})` }}
+          >
+            <Title
+              order={1}
+              sx={{
+                fontFamily: "'Lato', sans-serif",
+                fontWeight: "600",
+                fontSize: "2rem",
+              }}
+            >
+              {hotel.name}
+            </Title>
+            <Text style={{ maxWidth: "min(45ch,100%)" }}>
+              {hotel.description}
+            </Text>
+            <Text>{`${hotel.address.city}, ${hotel.address.state} - ${hotel.address.country}`}</Text>
+            <Group>
+              <Text>rating: {hotel.ratings}</Text>
+              <Text>rooms available: {hotel.roomsAvailable}</Text>
+            </Group>
+            {createImageGrid(hotel)}
+            <Button color={"teal"} size={"md"}>
+              Book Now
+            </Button>
+          </Stack>
+          <Space my={50} />
+          <Grid>
+            <Grid.Col span={6}>
+              <Text color="dimmed" size={"lg"} mb={"xl"}>
+                Packages
+              </Text>
+              <Group>
+                {hotel.hotelPackages.map((pack, idx) => {
+                  return (
+                    <Card
+                      key={idx}
+                      shadow={"md"}
+                      p="lg"
+                      radius={8}
+                      withBorder={true}
+                    >
+                      <Group>
+                        <Text color={"indigo"} size={"xl"}>
+                          ${pack.price}
+                        </Text>
+                        <Text transform={"uppercase"} size={"sm"}>
+                          {pack.package}
+                        </Text>
+                      </Group>
+                      <Text size={"sm"} style={{ width: "min(30ch,100%)" }}>
+                        {pack.description}
+                      </Text>
+                    </Card>
+                  );
+                })}
+              </Group>
+            </Grid.Col>
+            <Grid.Col span={6}>
+              <Stack align="flex-end" justify="flex-start" spacing="xs">
+                <Text color="dimmed" size={"lg"} mb={"xl"}>
+                  Comments
+                </Text>
+                {hotel.comments && hotel.comments.length !== 0 ? (
+                  hotel.comments.map((comment, idx) => {
+                    return (
+                      <Paper
+                        withBorder
+                        radius="md"
+                        key={idx}
+                        sx={(theme) => ({
+                          width: "min(280px,100%)",
+                          padding: `${theme.spacing.lg}px ${theme.spacing.xl}px`,
+                        })}
+                      >
+                        <Group>
+                          <Text size="sm">{comment.user?.fullName}</Text>
+                          <Text size="xs" color="dimmed">
+                            rating:{comment.rating}
+                          </Text>
+                        </Group>
+                        <Text
+                          sx={(theme) => ({
+                            paddingTop: theme.spacing.sm,
+                          })}
+                          size="sm"
+                        >
+                          {comment.comment}
+                        </Text>
+                      </Paper>
+                    );
+                  })
+                ) : (
+                  <Text
+                    sx={(theme) => ({
+                      paddingTop: theme.spacing.xl,
+                    })}
+                    size="sm"
+                    color="dimmed"
+                  >
+                    No Comments Available
+                  </Text>
+                )}
+              </Stack>
+            </Grid.Col>
+          </Grid>
         </Box>
       ) : (
         <></>
