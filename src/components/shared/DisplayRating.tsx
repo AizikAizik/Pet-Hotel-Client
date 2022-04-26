7acb13821ce553252a680abb23bf99f2b725249f
Eniola Ajayi <ajayieniola16@gmail.com>
2022-04-26 05:14:15 +0100

    create display rating component

---
+++ src/components/shared/DisplayRating.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Rating } from "react-simple-star-rating";
+interface DisplayRatingProps {
+  value: number;
+  size: number;
+}
+export default function DisplayRating(props: DisplayRatingProps) {
+  return (
+    <Rating
+      initialValue={0}
+      size={props.size ? props.size : 22}
+      readonly={true}
+      ratingValue={props.value}
+    />
+  );
+}
